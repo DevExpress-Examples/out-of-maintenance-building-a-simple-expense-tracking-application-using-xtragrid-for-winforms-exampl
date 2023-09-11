21b219beebe43d7145e6e4d9cb1511688a830ed6
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-09-11 14:20:47 +0400

    Change example status to out of maintenance

--- CS/DataLayer/Data/Classes/TransactionType.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace DataLayer.Data.Classes
-{
-    public enum TransactionType { Income, Expense }
-}
